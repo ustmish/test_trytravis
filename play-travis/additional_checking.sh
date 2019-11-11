162d16aa04c622bda50c7303251f3c1aca34045b
root <root@devengine2.dev>
2019-11-11 23:19:32 +0300

    ansible-3 add trytravis testing 2

--- play-travis/additional_checking.sh
+++ play-travis/additional_checking.sh
@@ -1,24 +1,34 @@
+echo Download terraform:
 curl https://releases.hashicorp.com/terraform/0.12.13/terraform_0.12.13_linux_amd64.zip -o terraform_0.12.13_linux_amd64.zip
+echo unzip terraform:
 unzip terraform_0.12.13_linux_amd64.zip
 cp terraform/ /usr/bin
 
+echo Download packer
 curl https://releases.hashicorp.com/packer/1.4.5/packer_1.4.5_linux_amd64.zip -o packer_1.4.5_linux_amd64.zip
+
+echo unzip packer
 unzip packer_1.4.5_linux_amd64.zip
 cp pakcer /usr/bin
 
+echo install ansible-lint
 pip install ansible-lint
 
+echo Download tflint
 $ curl -L "$(curl -s https://api.github.com/repos/terraform-linters/tflint/releases/latest | grep -o -E "https://.+?_linux_amd64.zip")" > tflint.zip && unzip tflint.zip && rm tflint.zip
 
 cp tflint /usr/bin
 
+echo packer checking
 cd packer
 packer validate -var-file=./variables.json app.json
 packer validate -var-file=./variables.json db.json
 
+echo terraform checking
 cd ../terraform
 terraform validate
 tflint
 
+echo anible-lint checking
 cd ../ansible/playbooks/
 ansible-lint *
