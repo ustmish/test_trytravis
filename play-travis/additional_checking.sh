d2bba05a253ea9822c51ec56f04a0350f2451a26
root <root@devengine2.dev>
2019-11-12 11:57:15 +0300

    ansible3 20191112 1

--- play-travis/additional_checking.sh
+++ play-travis/additional_checking.sh
@@ -1,15 +1,13 @@
 echo Download terraform:
-curl https://releases.hashicorp.com/terraform/0.12.13/terraform_0.12.13_linux_amd64.zip -o terraform_0.12.13_linux_amd64.zip
+curl https://releases.hashicorp.com/terraform/0.12.13/terraform_0.12.13_linux_amd64.zip -o /tmp/terraform_0.12.13_linux_amd64.zip
 echo unzip terraform:
-unzip terraform_0.12.13_linux_amd64.zip
-cp terraform/ /usr/bin
+unzip /tmp/terraform_0.12.13_linux_amd64.zip -d /usr/bin
 
 echo Download packer
-curl https://releases.hashicorp.com/packer/1.4.5/packer_1.4.5_linux_amd64.zip -o packer_1.4.5_linux_amd64.zip
+curl https://releases.hashicorp.com/packer/1.4.5/packer_1.4.5_linux_amd64.zip -o /tmp/packer_1.4.5_linux_amd64.zip
 
 echo unzip packer
-unzip packer_1.4.5_linux_amd64.zip
-cp pakcer /usr/bin
+unzip /tmp/packer_1.4.5_linux_amd64.zip -d /usr/bin
 
 echo install ansible-lint
 pip install ansible-lint
