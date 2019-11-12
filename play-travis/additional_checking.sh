6aaed6d2119105b3f01b46104966389cbfe78b13
root <root@devengine2.dev>
2019-11-12 12:12:04 +0300

    ansible4 20191112 2

--- play-travis/additional_checking.sh
+++ play-travis/additional_checking.sh
@@ -1,32 +1,32 @@
-echo Download terraform:
-curl https://releases.hashicorp.com/terraform/0.12.13/terraform_0.12.13_linux_amd64.zip -o /tmp/terraform_0.12.13_linux_amd64.zip
-echo unzip terraform:
-unzip /tmp/terraform_0.12.13_linux_amd64.zip -d /usr/bin
+#echo Download terraform:
+#curl https://releases.hashicorp.com/terraform/0.12.13/terraform_0.12.13_linux_amd64.zip -o /tmp/terraform_0.12.13_linux_amd64.zip
+#echo unzip terraform:
+#unzip /tmp/terraform_0.12.13_linux_amd64.zip -d /usr/bin
 
 echo Download packer
 curl https://releases.hashicorp.com/packer/1.4.5/packer_1.4.5_linux_amd64.zip -o /tmp/packer_1.4.5_linux_amd64.zip
 
-echo unzip packer
-unzip /tmp/packer_1.4.5_linux_amd64.zip -d /usr/bin
+#echo unzip packer
+#unzip /tmp/packer_1.4.5_linux_amd64.zip -d /usr/bin
 
-echo install ansible-lint
-pip install ansible-lint
+#echo install ansible-lint
+#pip install ansible-lint
 
-echo Download tflint
-$ curl -L "$(curl -s https://api.github.com/repos/terraform-linters/tflint/releases/latest | grep -o -E "https://.+?_linux_amd64.zip")" > tflint.zip && unzip tflint.zip && rm tflint.zip
+#echo Download tflint
+#$ curl -L "$(curl -s https://api.github.com/repos/terraform-linters/tflint/releases/latest | grep -o -E "https://.+?_linux_amd64.zip")" > tflint.zip && unzip tflint.zip && rm tflint.zip
 
-cp tflint /usr/bin
+#cp tflint /usr/bin
 
-echo packer checking
-cd packer
-packer validate -var-file=./variables.json app.json
-packer validate -var-file=./variables.json db.json
+#echo packer checking
+#cd packer
+#packer validate -var-file=./variables.json app.json
+#packer validate -var-file=./variables.json db.json
 
-echo terraform checking
-cd ../terraform
-terraform validate
-tflint
+#echo terraform checking
+#cd ../terraform
+#terraform validate
+#tflint
 
-echo anible-lint checking
-cd ../ansible/playbooks/
-ansible-lint *
+#echo anible-lint checking
+#cd ../ansible/playbooks/
+#ansible-lint *
